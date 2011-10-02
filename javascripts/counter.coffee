581e2f744da4272e813a7a55e4be12e9709424ad
Nick Quaranto <nick@thoughtbot.com>
2011-10-02 12:06:10 -0400

    scale it up bro

--- javascripts/counter.coffee
+++ javascripts/counter.coffee
@@ -1,2 +1,25 @@
+# based on http://www.spookandpuff.com/examples/dynamicTextSize.html
+
+class BodyScaler
+  scaleFactor: 0.35
+  maxScale: 400
+  minScale: 30
+
+  constructor: ->
+    @body = $("body")
+
+  scale: =>
+    scaleSource = @body.width()
+    fontSize = scaleSource * @scaleFactor
+
+    if (fontSize > @maxScale)
+      fontSize = @maxScale
+    if (fontSize < @minScale)
+      fontSize = @minScale
+
+    @body.css("font-size", "#{fontSize}%")
+
 $ ->
-  console.log("started!")
+  scaler = new BodyScaler()
+  scaler.scale()
+  $(window).resize(scaler.scale)
--- javascripts/counter.js
+++ javascripts/counter.js
@@ -1,5 +1,31 @@
 (function() {
+  var BodyScaler;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  BodyScaler = (function() {
+    BodyScaler.prototype.scaleFactor = 0.35;
+    BodyScaler.prototype.maxScale = 400;
+    BodyScaler.prototype.minScale = 30;
+    function BodyScaler() {
+      this.scale = __bind(this.scale, this);      this.body = $("body");
+    }
+    BodyScaler.prototype.scale = function() {
+      var fontSize, scaleSource;
+      scaleSource = this.body.width();
+      fontSize = scaleSource * this.scaleFactor;
+      if (fontSize > this.maxScale) {
+        fontSize = this.maxScale;
+      }
+      if (fontSize < this.minScale) {
+        fontSize = this.minScale;
+      }
+      return this.body.css("font-size", "" + fontSize + "%");
+    };
+    return BodyScaler;
+  })();
   $(function() {
-    return console.log("started!");
+    var scaler;
+    scaler = new BodyScaler();
+    scaler.scale();
+    return $(window).resize(scaler.scale);
   });
 }).call(this);
