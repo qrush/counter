5e3356750cff1cbc080f13aacc501b3b6e1cc7bd
Nick Quaranto <nick@thoughtbot.com>
2011-10-03 08:16:46 -0400

    getting there

--- javascripts/counter.coffee
+++ javascripts/counter.coffee
@@ -19,10 +19,32 @@ class BodyScaler
 
     @body.css("font-size", "#{fontSize}%")
 
+class Counter
+  points:
+    blue: 0
+    green: 0
+    red: 0
+    yellow: 0
+    black: 0
+    gray: 0
+
+  count: (event) =>
+    console.log("LOL")
+    form = $(event.target)
+    color = form.siblings("h2").text()
+    counter = form.siblings("h3")
+    @points[color] += form.find("input").val()
+    counter.text(@points[color])
+    false
+
+
 class Score
   points: 0
 
 $ ->
   scaler = new BodyScaler()
+  counter = new Counter()
+
   scaler.scale()
   $(window).resize(scaler.scale)
+  $("form").submit(counter.count)
--- javascripts/counter.js
+++ javascripts/counter.js
@@ -1,5 +1,5 @@
 (function() {
-  var BodyScaler, Score;
+  var BodyScaler, Counter, Score;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   BodyScaler = (function() {
     BodyScaler.prototype.scaleFactor = 0.35;
@@ -22,15 +22,41 @@
     };
     return BodyScaler;
   })();
+  Counter = (function() {
+    function Counter() {
+      this.count = __bind(this.count, this);
+    }
+    Counter.prototype.points = {
+      blue: 0,
+      green: 0,
+      red: 0,
+      yellow: 0,
+      black: 0,
+      gray: 0
+    };
+    Counter.prototype.count = function(event) {
+      var color, counter, form;
+      console.log("LOL");
+      form = $(event.target);
+      color = form.siblings("h2").text();
+      counter = form.siblings("h3");
+      this.points[color] += form.find("input").val();
+      counter.text(this.points[color]);
+      return false;
+    };
+    return Counter;
+  })();
   Score = (function() {
     function Score() {}
     Score.prototype.points = 0;
     return Score;
   })();
   $(function() {
-    var scaler;
+    var counter, scaler;
     scaler = new BodyScaler();
+    counter = new Counter();
     scaler.scale();
-    return $(window).resize(scaler.scale);
+    $(window).resize(scaler.scale);
+    return $("form").submit(counter.count);
   });
 }).call(this);
